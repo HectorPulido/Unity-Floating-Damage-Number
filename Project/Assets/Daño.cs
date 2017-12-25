e637e4490db8313ad299fa6fb462209c0c4d9e26
Hector Pulido <Hectorandrespp@gmail.com>
2017-12-25 01:49:57 -0500

    First Commit

---
+++ Project/Assets/Daño.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daño : MonoBehaviour {
+
+    public Animator anim;
+    public GameObject numeroPrefab;
+
+    void InstanciarNumero()
+    {
+        GameObject go = Instantiate(numeroPrefab, transform.position + Random.onUnitSphere *0.5f, Quaternion.identity) as GameObject;
+        go.GetComponent<Numero>().Inicializar(Random.Range(1, 10));
+    }
+
+    void OnMouseDown()
+    {
+        anim.SetTrigger("Daño");
+        InstanciarNumero();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Unity Floating Damage Number
+
+A example of a floating damage number on Unity, Perfect for a RPG
+The project is coded with C#
+
+This tutorial was made for Hector Pulido for his youtube channel 
+https://www.youtube.com/channel/UCS_iMeH0P0nsIDPvBaJckOw
+
+Please consider Support on Patreon 
+https://www.patreon.com/HectorPulido
